ad853fd0b7e126fc86c5306cb5eb5bdc56186e92
Andrew Hankins <67705231+AndrewH-14@users.noreply.github.com>
2023-07-19 08:04:10 -0400

    Worked on updates

---
+++ sketch/button.ino
@@ -0,0 +1,26 @@
+/**
+ * File that will implement all of the Makeblock's LED functionality.
+*/
+#include "button.h"
+
+#define BUTTON_PIN 2
+
+/**
+ * Determines if the button is being pressed.
+ *
+ * @retval true The button is being pressed
+ * @retval false The button is not being pressed
+ */
+bool button_is_pressed(void) {
+    // initialize the pushbutton pin as an input:
+    pinMode(BUTTON_PIN, INPUT);
+    // Read the state of the button
+    int button_state = digitalRead(BUTTON_PIN);
+    // Determine if the button is being pressed
+    if (HIGH == button_state) {
+        return true;
+    }
+    else  {
+        return false;
+    }
+}
--- sketch/state_line_follower.ino
+++ sketch/state_line_follower.ino
@@ -6,10 +6,47 @@
 #include "state_line_follower.h"
 
 #include "motor.h"
+#include "button.h"
+#include "src/MeSingleLineFollower.h"
 #include <MeMegaPi.h>
 
-// TODO: Determine coorect port to use
-MeLineFollower lineFinder(PORT_3);
+// Define the ports the the line follower peripherals are on
+#define LINEFLOOWER_S1_PIN  A9
+#define LINEFLOOWER_S2_PIN  A10
+
+// Initialize the line follower sensors
+MeSingleLineFollower line_follower_s1(LINEFLOOWER_S1_PIN);
+MeSingleLineFollower line_follower_s2(LINEFLOOWER_S2_PIN);
+
+// Line following sensor state
+typedef enum {
+    SEN1_IN_SEN2_IN,
+    SEN1_OUT_SEN2_IN,
+    SEN1_IN_SEN2_OUT,
+    SEN1_OUT_SEN2_OUT
+} LineFollowingStates;
+
+/**
+ * Gets the current state of the sensors.
+*/
+LineFollowingStates get_state(void) {
+    int s1 = line_follower_s1.readSensor();
+    int s2 = line_follower_s2.readSensor();
+    LineFollowingStates state = SEN1_IN_SEN2_IN;
+   if (s1 == 1 && s2 == 1) {
+        state = SEN1_IN_SEN2_IN;
+    }
+    else if (s1 == 0 && s2 == 1) {
+        state = SEN1_OUT_SEN2_IN;
+    }
+    else if (s2 == 1 && s2 == 0) {
+        state = SEN1_IN_SEN2_OUT;
+    }
+    else {
+        state = SEN1_OUT_SEN2_OUT;
+    }
+    return state;
+}
 
 /**
  * The start point for the line follower 1 & 2 states. This function will return
@@ -19,23 +56,23 @@ MeLineFollower lineFinder(PORT_3);
  * @retval false An error occured.
 */
 bool state_line_follower_start(void) {
-    int sensor_state = lineFinder.readSensors();
+    LineFollowingStates sensor_state = get_state();
     while (sensor_state != S1_OUT_S2_OUT) {
-        sensor_state = lineFinder.readSensors();
+        sensor_state = get_state();
         switch (sensor_state) {
-            case S1_IN_S2_IN:
+            case SEN1_IN_SEN2_IN:
                 // Continue straight
                 motor_move_forward(100, 100);
                 break;
-            case S1_IN_S2_OUT:
+            case SEN1_IN_SEN2_OUT:
                 // Turn left
                 motor_turn_left(100, 50);
                 break;
-            case S1_OUT_S2_IN:
+            case SEN1_OUT_SEN2_IN:
                 // Turn right
                 motor_turn_right(100, 50);
                 break;
-            case S1_OUT_S2_OUT:
+            case SEN1_OUT_SEN2_OUT:
             default:
                 break;
         }
