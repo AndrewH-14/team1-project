fd527efdd43fe2ea7da3c42b41124bb7871410e9
Nafiz Ahmed <nafizahmed127@gmail.com>
2023-08-01 01:22:00 -0400

    Feat: GUI now displays the different graphs. (test.py is the working file)

--- mission_gui/gui.py
+++ mission_gui/gui.py
@@ -4,6 +4,8 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
 import csv
 
+global flag
+
 # Read the barrier synthetic dataset
 barrier_file = open('barrier.dat', 'r')
 reader = csv.reader(barrier_file)
@@ -26,61 +28,89 @@ for record in reader:
     collision_dat.append(record)
 
 # Combined graph data variables
-graph_width  = 1000
-graph_height = 300
+graph_width  = 800
+graph_height = 400
 graph_bl     = (0, 0)
 graph_tr     = (len(collision_dat), len(collision_dat))
 pad_xy       = (0, 10)
 
 # Create the frist header which will control input values
-buttons1    = [sg.Button('Barrier 1'), sg.Button('Line 1'), sg.Button('Collision 2')]
+buttons1    = [sg.Button('Barrier 1'), sg.Button('Line 1'), sg.Button('Collision 1')]
 start_time1 = [sg.Text('Start Time 1 (hh:mm):'), sg.In(size=5)]
 end_time1   = [sg.Text('End Time 1 (hh:mm):'), sg.In(size=5)]
 header1     = buttons1 + start_time1 + end_time1
 
 # Create the first graph which will display a single dataset
-graph1 = [sg.Graph(canvas_size=(graph_width, graph_height), graph_bottom_left=graph_bl, graph_top_right=graph_tr, pad=pad_xy, expand_x=True)]
+graph1 = [sg.Canvas(size=(graph_width, graph_height), key='-CANVAS-')]
 
 # Create the second header
 # Create the frist header which will control input values
-buttons2    = [sg.Button('Barrier 2'), sg.Button('Line 2'), sg.Button('Collision 2')]
-start_time2 = [sg.Text('Start Time 2 (hh:mm):'), sg.In(size=5)]
-end_time2   = [sg.Text('End Time 2 (hh:mm):'), sg.In(size=5)]
-header2     = buttons2 + start_time2 + end_time2
+# buttons2    = [sg.Button('Barrier 2'), sg.Button('Line 2'), sg.Button('Collision 2')]
+# start_time2 = [sg.Text('Start Time 2 (hh:mm):'), sg.In(size=5)]
+# end_time2   = [sg.Text('End Time 2 (hh:mm):'), sg.In(size=5)]
+# header2     = buttons2 + start_time2 + end_time2
 
 # Create the first graph which will display a single dataset
-graph2 = [sg.Graph(canvas_size=(graph_width, graph_height), graph_bottom_left=graph_bl, graph_top_right=graph_tr, pad=pad_xy, expand_x=True)]
+#graph2 = [sg.Canvas(size=(graph_width, graph_height), key='-CANVAS-')]
 
 # All the stuff inside your window.
 layout = [ [sg.Text("MakeBlock Mission Data", expand_x=True, justification='center', font=('Arial', 30), background_color='Black')],
            [sg.Text("Sensor 1 Readings:", font=('Arial', 20), background_color="Black", pad=10)],
-           header1, graph1,
-           [sg.Text("Sensor 2 Readings:", font=('Arial', 20), background_color="Black", pad=10)],
-           header2, graph2 ]
+           header1, graph1 ]
+        #    [sg.Text("Sensor 2 Readings:", font=('Arial', 20), background_color="Black", pad=10)],
+        #    header2, graph2 ]
 
+flag = False
 # Function to create the graph based on the selected button
 def create_graph(button_num):
+    global flag
+    print(flag)
+    if flag == True:
+        ax.clear()
     # Data for the graph (You can replace this with your own data)
-    x = np.linspace(0, 10, 100)
-    y = np.sin(button_num * x)
+    x = np.linspace(1, 12, 12)
     
     if button_num == 1:
         # Display Barrier Data
+        flag = True
         data_type = 'Barrier Sensors'
+        y1 = [0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0]
+        y2 = [0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0]
+        y3 = [0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0]
+        fig, ax = plt.subplots(figsize=(5, 4))
+        ax.plot(x, y1, marker='o', color='b', label='Line 1')
+        ax.plot(x, y2, marker='s', color='r', label='Line 2')
+        ax.plot(x, y3, marker='^', color='g', label='Line 3')
     elif button_num == 2:
         # Display Line Data
+        flag = True
         data_type = 'Line Following Sensors'
+        y1 = [1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0]
+        y2 = [1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1]
+        fig, ax = plt.subplots(figsize=(5, 4))
+        ax.plot(x, y1, marker='o', color='b', label='Line 1')
+        ax.plot(x, y2, marker='s', color='r', label='Line 2')
     elif button_num == 3:
         # Display Line Data
+        flag = True
         data_type = 'Collision Sensors'
+        y1 = [0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0]
+        y2 = [0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0]
+        fig, ax = plt.subplots(figsize=(5, 4))
+        ax.plot(x, y1, marker='o', color='b', label='Line 1')
+        ax.plot(x, y2, marker='s', color='r', label='Line 2')
 
     # Create the figure and plot the graph
-    fig, ax = plt.subplots()
-    ax.plot(x, y)
-    ax.set_xlabel('Time')
-    ax.set_ylabel('Sensor Reading')
-    ax.set_title(f'Line graph for the {data_type}')
+    ax.set_xlabel('Time (Seconds)', fontsize=10)
+    ax.set_ylabel('Sensor Reading', fontsize=10)
+    ax.set_title(f'Line graph for the {data_type}', fontsize=9)
+    ax.tick_params(axis='x', labelsize=6)  # Set the font size for the x-axis tick labels
+    ax.tick_params(axis='y', labelsize=6) 
     ax.grid(True)
+    ax.legend()
+    
+    # Set x-axis ticks to show each number from 1 through 12
+    ax.set_xticks(x)
 
     # Clear previous drawings on the Matplotlib element
     canvas_elem.TKCanvas.delete("all")
@@ -101,11 +131,11 @@ canvas_elem = window['-CANVAS-']
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == 'Barrier':
+    if event == 'Barrier 1':
         create_graph(1)
-    if event == 'Line':
+    if event == 'Line 1':
         create_graph(2)
-    if event == 'Collision':
+    if event == 'Collision 1':
         create_graph(3)
     if event == sg.WIN_CLOSED:
         break
--- mission_gui/test.py
+++ mission_gui/test.py
@@ -2,53 +2,88 @@ import PySimpleGUI as sg
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import numpy as np
+import io
 
-# Function to create the graph based on the selected button
-def create_graph(button_num):
+# Create the frist header which will control input values
+buttons1 = [sg.Button('Barrier', font=('Arial', 16)), sg.Button('Line', font=('Arial', 16)), sg.Button('Collision', font=('Arial', 16))]
+start_time1 = [sg.Text('Start Time 1 (hh:mm):', font=('Arial', 14)), sg.In(size=8)]
+end_time1 = [sg.Text('End Time 1 (hh:mm):', font=('Arial', 14)), sg.In(size=8)]
+header1 = buttons1 + start_time1 + end_time1
+
+# Create the first graph which will display a single dataset
+graph1 = [sg.Image(filename='', key='-IMAGE-')]
+
+# All the stuff inside your window.
+layout = [[sg.Text("MakeBlock Mission Data", expand_x=True, justification='center', font=('Arial', 30), background_color='Black')],
+          [sg.Text("Sensor Readings:", font=('Arial', 20), background_color="Black", pad=10)],
+          header1, graph1]
+
+# Create the Window
+sg.theme('Black')
+window = sg.Window('Mission Data', layout, resizable=True, margins=(20, 20), finalize=True)  # Add finalize=True parameter
+
+# Function to update the graph based on the selected button
+def update_graph(button_num):
     # Data for the graph (You can replace this with your own data)
-    x = np.linspace(0, 12, 100)
-    y = np.sin(button_num * x)
+    x = np.linspace(1, 12, 12)
+
+    if button_num == 1:
+        # Display Barrier Data
+        data_type = 'Barrier Sensors'
+        y1 = [0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0]
+        y2 = [0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0]
+        y3 = [0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0]
+        labels = ['Sensor 1', 'Sensor 2', 'Sensor 3']
+    elif button_num == 2:
+        # Display Line Data
+        data_type = 'Line Following Sensors'
+        y1 = [1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0]
+        y2 = [0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1]
+        labels = ['Sensor 1', 'Sensor 2']
+    elif button_num == 3:
+        # Display Line Data
+        data_type = 'Collision Sensors'
+        y1 = [0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0]
+        y2 = [0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0]
+        labels = ['Sensor 1', 'Sensor 2']
 
     # Create the figure and plot the graph
-    fig, ax = plt.subplots()
-    ax.plot(x, y)
-    ax.set_xlabel('X-axis')
-    ax.set_ylabel('Y-axis')
-    ax.set_title(f'Line graph for Button {button_num}')
+    fig, ax = plt.subplots(figsize=(10, 7))  # Change the figsize to (8, 6)
+    ax.plot(x, y1, marker='o', color='b', label='Line 1')
+    ax.plot(x, y2, marker='s', color='r', label='Line 2')
+
+    if button_num == 1:
+        ax.plot(x, y3, marker='^', color='g', label='Line 3')
+
+    ax.set_xlabel('Time (Seconds)', fontsize=14)
+    ax.set_ylabel('Sensor Reading', fontsize=14)
+    ax.set_title(f'Line graph for the {data_type}', fontsize=16)
+    ax.tick_params(axis='x', labelsize=12)
+    ax.tick_params(axis='y', labelsize=12)
     ax.grid(True)
+    ax.legend(labels, title='Legend')
+    ax.set_xticks(x)
 
-    # Clear previous drawings on the Matplotlib element
-    canvas_elem.TKCanvas.delete("all")
+    # Convert the Matplotlib figure to an image
+    figure_to_image(fig, '-IMAGE-')
 
-    # Draw the figure on the Matplotlib element
-    figure_canvas_agg = FigureCanvasTkAgg(fig, master=canvas_elem.TKCanvas)
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
+def figure_to_image(fig, key):
+    # Convert a Matplotlib figure to an image
+    buf = io.BytesIO()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    window[key].update(data=buf.getvalue())
 
-# Define the layout for the GUI
-layout = [
-    [sg.Button('Button 1'), sg.Button('Button 2'), sg.Button('Button 3')],
-    [sg.Canvas(size=(400, 300), key='-CANVAS-')],
-]
-
-# Create the window
-window = sg.Window('Line Graph with PySimpleGUI', layout)
-
-# Get the Matplotlib element from the layout
-canvas_elem = window['-CANVAS-']
-
-# Event loop
+# Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-
-    if event == sg.WINDOW_CLOSED:
+    if event == sg.WIN_CLOSED:
         break
-    elif event == 'Button 1':
-        create_graph(1)
-    elif event == 'Button 2':
-        create_graph(2)
-    elif event == 'Button 3':
-        create_graph(3)
+    if event == 'Barrier':
+        update_graph(1)
+    if event == 'Line':
+        update_graph(2)
+    if event == 'Collision':
+        update_graph(3)
 
-# Close the window and exit the GUI
 window.close()
